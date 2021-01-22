dea624a179346a3d88ae5687a267f65795d6e1a2
Dovi Geretz <dovi@geretz.com>
2021-01-21 21:38:53 -0500

    contact form using modal

--- portfolio/assets/js/popup.js
+++ portfolio/assets/js/popup.js
@@ -1,14 +1,16 @@
-$(document).ready(function () {
-    $(".popup").hide();
-    $(".openpop").click(function (e) {
-        e.preventDefault();
-        $("iframe").attr("src", $(this).attr('href'));
-        $(".links").fadeOut('slow');
-        $(".popup").fadeIn('slow');
-    });
 
-    $(".close").click(function () {
-        $(this).parent().fadeOut("slow");
-        $(".links").fadeIn("slow");
-    });
+
+$(document).ready(function () {
+  $(".popup").hide();
+  $(".openpop").click(function (e) {
+      e.preventDefault();
+      $("iframe").attr("src", $(this).attr('href'));
+      $(".links").fadeOut('slow');
+      $(".popup").fadeIn('slow');
+  });
+
+  $(".close").click(function () {
+      $(this).parent().fadeOut("slow");
+      $(".links").fadeIn("slow");
+  });
 });
