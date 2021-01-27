20d07d785b35b9fa0cf2e3af99f114d8085d940e
Dovi Geretz <dovi@geretz.com>
2021-01-26 22:33:21 -0500

    etsy map revune counter

--- SellerMap/map.js
+++ SellerMap/map.js
@@ -1,10 +1,12 @@
 
 
 var map;
-var mapLat = 39.50;
+var mapLat = 37.50;
 var mapLng = -98.35;
 var mapDefaultZoom = 5.5;
 var ordercount = 0;
+var grandTotal = 0;
+initialize_map();
 
 
 
@@ -27,8 +29,9 @@ function initialize_map() {
   });
 }
 
-function add_map_point(lat, lng, date) {
+function add_map_point(lat, lng, date, ordercount, totalX) {
 
+  
   var dateObject = new Date(date);
 
   var month = (dateObject.getMonth()) + 1;
@@ -54,19 +57,53 @@ function add_map_point(lat, lng, date) {
 
   setInterval(function run() {
     map.addLayer(vectorLayer);
-    console.log(date);
+    grandTotal = grandTotal + totalX;
+    var n = grandTotal.toFixed(2);
+    var res = n.split("");
 
-  }, 3000);
+if(res.length===5){
+  res.unshift("0", "0", "0", "0");
+}
+else if(res.length===6){
+  res.unshift("0", "0", "0");
+}
+else if(res.length===7){
+  res.unshift( "0", "0");
+}
+else if(res.length===8){
+  res.unshift("0");
+}
+
+
+
+console.log(res);
+
+document.getElementById('cents1').innerText = res[8];
+document.getElementById('cents10').innerText =res[7];
+document.getElementById('dot').innerText = res[6];
+document.getElementById('dollar1').innerText =res[5];
+document.getElementById('dollar10').innerText = res[4];
+document.getElementById('dollar100').innerText =res[3];
+document.getElementById('dollar1000').innerText = res[2];
+document.getElementById('dollar10000').innerText =res[1];
+document.getElementById('dollar100000').innerText =res[0];
+document.getElementById('date').innerText = "As of " +date + "Your total revune is  ";
+    
+
+
+  }, ordercount * 30);
 
 
 }
+
 
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 const request = async () => {
   const response = await fetch("./etsy2020orders.json");
   const orders = await response.json();
-  initialize_map();
+
+
   console.log(orders);
 
 
@@ -85,48 +122,14 @@ const request = async () => {
     country = (order.country);
     total = (order.total);
 
-    add_map_point(lat, long, date, ordercount);
+
+
+    add_map_point(lat, long, date, ordercount, total);
   });
 };
 
 
 request();
 
-// .then(r => {
-//   if (!r.ok) {
-//     throw new Error(`${r.status} ${r.statusText}`);
-//   }
-//   return r.json();
-// })
 
-// .then(orders => {
-
-
-
-//   orders.forEach(order => {
-
-// ordercount++;
-
-
-
-
-//     lat = (order.latitude);
-//     long = (order.longitude);
-//     date = (order.date);
-//     orderNumber = (order.order);
-//     id = (order.name);
-//     street = (order.street);
-//     city = (order.city);
-//     state = (order.state);
-//     zip = (order.zip);
-//     country = (order.country);
-//     total = (order.total);
-
-
-//    add_map_point(lat, long, date, ordercount);
-//   });
-
-
-
-  // });
 
